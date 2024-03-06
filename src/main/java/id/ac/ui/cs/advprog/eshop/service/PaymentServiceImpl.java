fcdb77dc3bad015bf3a94456b675abe5dbca902c
narendradzulqarnain <narendra.dz1107@gmail.com>
2024-03-06 20:45:21 +0700

    [GREEN] implement (belom berhasil) paymentserviceimpl

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -2,29 +2,42 @@ package id.ac.ui.cs.advprog.eshop.service;
 
 import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
+import id.ac.ui.cs.advprog.eshop.repository.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
+
 @Service
 public class PaymentServiceImpl implements PaymentService{
+    Map<Payment,Order> paymentOrderMap = new HashMap<>();
+    @Autowired
+    PaymentRepository paymentRepository;
     @Override
     public Payment addPayment(Order order, String method, Map<String, String> paymentData) {
-        return null;
+        Payment payment = new Payment(UUID.randomUUID().toString(), method, "", paymentData);
+        Payment result = paymentRepository.save(payment);
+        paymentOrderMap.put(result, order);
+        return payment;
     }
 
     @Override
     public Payment setStatus(Payment payment, String status) {
-        return null;
+        Payment temp = paymentRepository.findById(payment.getId());
+        Payment result = paymentRepository.save(new Payment(payment.getId(), payment.getMethod(), temp.getStatus(),payment.getPaymentData()));
+        return result;
     }
 
     @Override
     public Payment getPayment(String paymentId) {
-        return null;
+        return paymentRepository.findById(paymentId);
     }
 
     @Override
     public List<Payment> getAllPayments() {
-        return null;
+        return paymentRepository.findAll();
     }
 }
