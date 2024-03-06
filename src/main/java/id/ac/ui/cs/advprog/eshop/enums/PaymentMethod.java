b8dee6917d9da75add5bf972071d6e2f9657c2a7
narendradzulqarnain <narendra.dz1107@gmail.com>
2024-03-06 18:44:09 +0700

    [REFACTOR] Add PaymentMethod enum

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/enums/PaymentMethod.java
@@ -0,0 +1,19 @@
+package id.ac.ui.cs.advprog.eshop.enums;
+
+public enum PaymentMethod {
+    VOUCHER_CODE("VoucherCode"),
+    CASH_ON_DELIVERY("CashOnDelivery");
+
+    private final String value;
+    private PaymentMethod(String value) {this.value = value;}
+
+    public static boolean contains(String param) {
+        for (PaymentMethod paymentMethod : PaymentMethod.values()) {
+            if (paymentMethod.value.equals(param)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
